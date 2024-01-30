3aa3d0c1a767c6bf0329a9eaee33a001f71a84af
Anderson Padoan <andy_paduano@hotmail.com>
2024-01-30 15:27:27 -0300

    create gameboard

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+print("           Battleship Game          ")
+print("   0   1   2   3   4   5   6   7   8   9")
+
 for x in range(10):
-    print(x)
+    print(x," _  " * 10)
+    
