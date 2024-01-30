a6410e17f0e8a6f0b441542961574896c1a23393
Anderson Padoan <andy_paduano@hotmail.com>
2024-01-30 15:49:21 -0300

    create function to show the game board

--- run.py
+++ run.py
@@ -2,9 +2,31 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
-print("           Battleship Game          ")
-print("   0   1   2   3   4   5   6   7   8   9")
-
-for x in range(10):
-    print(x," _  " * 10)
     
+
+def game_board(hit,miss,comp):
+    print("           Battleship Game          ")
+    print("   0   1   2   3   4   5   6   7   8   9")
+
+    place = 0
+
+    for x in range(10):
+        row = ""
+        for y in range(10):
+            ch = " _  "
+            if place in miss:
+                ch = " x  "
+            elif place in hit:
+                ch = " o  "
+            elif place in comp:
+                ch = " @  "
+            row = row + ch
+            place = place + 1
+        print(x, row)
+    
+    
+miss = [20,45,50]
+hit = [10,25,33]
+comp = [43,6]
+
+game_board(miss,hit,comp)
