f003a1e349813dfbd95f2c54d98150ca450360b3
Anderson Padoan <andy_paduano@hotmail.com>
2024-02-03 14:18:36 -0300

    test random boats

--- randomboats.py
+++ randomboats.py
@@ -1,5 +1,20 @@
 from random import randrange
 
+def check_ok(boat):
+    
+    for i in range(len(boat)):
+        num = boat[i]
+        if num < 0 or num > 99:
+            boat = [-1]
+            break
+        elif num % 10 == 9 and i < len(boat)-1:
+            if boat[i+1] % 10 == 0:
+                boat = [-1]
+                break
+        
+    return boat
+
+
 def check_boat(b, start,dire):
     
     dire = 1
@@ -7,23 +22,29 @@ def check_boat(b, start,dire):
     if dire == 1:
         for i in range(b):
             boat.append(start - i*10)
-            print(start - i*10)
+            boat = check_ok(boat)
     elif dire == 2:
         for i in range(b):
             boat.append(start - i*10)
-            print(start - i*10)    
+            boat = check_ok(boat)    
     elif dire == 3:
         for i in range(b):
             boat.append(start - i*10)
-            print(start - i*10)   
+            boat = check_ok(boat)   
     elif dire == 4:
         for i in range(b):
             boat.append(start - i*10)
-            print(start - i*10)   
+            boat = check_ok(boat)       
+    return boat
 
-boats = [4]
+ships = []
+boats = [5,4]
 for b in boats:
-    boat_start = randrange(99)
-    boat_direction = randrange (1,4)
-    print(b,boat_start,boat_direction)
-    check_boat(b,boat_start,boat_direction)
+    boat = [-1]
+    while boat[0] == -1:
+        boat_start = randrange(99)
+        boat_direction = randrange (1,4)
+        print(b,boat_start,boat_direction)
+        boat = check_boat(b,boat_start,boat_direction)
+    ships.append(boat)
+    print(ships)
