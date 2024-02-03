9364868444c87c8a0ab80f411335aa297edc5588
Anderson Padoan <andy_paduano@hotmail.com>
2024-02-03 14:30:38 -0300

    testing random ships function

--- randomboats.py
+++ randomboats.py
@@ -40,16 +40,39 @@ def check_boat(b, start,dire,take):
             boat = check_ok(boat,take)       
     return boat
 
-take = []
-ships = []
-boats = [5,4,3,3,2,2]
-for b in boats:
-    boat = [-1]
-    while boat[0] == -1:
-        boat_start = randrange(99)
-        boat_direction = randrange (1,4)
-        print(b,boat_start,boat_direction)
-        boat = check_boat(b,boat_start,boat_direction,take)
-    ships.append(boat)
-    take = take + boat
-    print(ships)
+
+def create_boats():
+    take = []
+    ships = []
+    boats = [5,4,3,3,2,2]
+    for b in boats:
+        boat = [-1]
+        while boat[0] == -1:
+            boat_start = randrange(99)
+            boat_direction = randrange (1,4)
+            print(b,boat_start,boat_direction)
+            boat = check_boat(b,boat_start,boat_direction,take)
+        ships.append(boat)
+        take = take + boat
+        print(ships)
+        
+    return ships,take
+
+def game_board(take):
+    print("           Battleship Game          ")
+    print("   0   1   2   3   4   5   6   7   8   9")
+
+    place = 0
+    for x in range(10):
+        row = ""
+        for y in range(10):
+            ch = " _  "
+            if place in take:
+                ch = " x  "
+            row = row + ch
+            place = place + 1
+        
+        print(x, row)
+
+boats,take = create_boats()
+game_board(take)
