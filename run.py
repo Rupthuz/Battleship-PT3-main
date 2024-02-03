74e81fadbee54187195dad4525b80ce8636d7273
Anderson Padoan <andy_paduano@hotmail.com>
2024-02-03 14:02:04 -0300

    start add function to create boats random

--- run.py
+++ run.py
@@ -1,7 +1,27 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randrange
 
+def check_boat(b, start,dire):
+    
+    dire = 1
+    boat = []
+    if dire == 1:
+        for i in range(b):
+            boat.append(start - i*10)
+            print(start - i*10)
+    elif dire == 2:
+        for i in range(b):
+            boat.append(start - i*10)
+            print(start - i*10)    
+
+boats = [4]
+for b in boats:
+    boat_start = randrange(99)
+    boat_direction = randrange (1,4)
+    print(b,boat_start,boat_direction)
+    check_boat(b,boat_start,boat_direction)
 
 def getting_shot(guesses):
     
