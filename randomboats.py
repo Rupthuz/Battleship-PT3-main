79ef499f25daf80a8994498b990a54aaa91eecd8
Anderson Padoan <andy_paduano@hotmail.com>
2024-02-06 17:01:06 -0300

    add function to random shot also fix check hit function

--- randomboats.py
+++ randomboats.py
@@ -1,67 +1,43 @@
 from random import randrange
 
-def check_ok(boat,take):
-    
-    for i in range(len(boat)):
-        num = boat[i]
-        if num in take:
-            boat = [-1]
-            break
-        elif num < 0 or num > 99:
-            boat = [-1]
-            break
-        elif num % 10 == 9 and i < len(boat)-1:
-            if boat[i+1] % 10 == 0:
-                boat = [-1]
-                break
-        
+def check_ok(boat, take):
+    for num in boat:
+        if num in take or num < 0 or num > 99:
+            return [-1]
+        elif num % 10 == 9 and boat.index(num) < len(boat) - 1:
+            if boat[boat.index(num) + 1] % 10 == 0:
+                return [-1]
     return boat
 
 
-def check_boat(b, start,dire,take):
-    
+def check_boat(b, start, dire, take):
     dire = 1
     boat = []
-    if dire == 1:
+    if dire in [1, 2, 3, 4]:
         for i in range(b):
-            boat.append(start - i*10)
-            boat = check_ok(boat,take)
-    elif dire == 2:
-        for i in range(b):
-            boat.append(start - i*10)
-            boat = check_ok(boat,take)    
-    elif dire == 3:
-        for i in range(b):
-            boat.append(start - i*10)
-            boat = check_ok(boat,take)   
-    elif dire == 4:
-        for i in range(b):
-            boat.append(start - i*10)
-            boat = check_ok(boat,take)       
+            boat.append(start - i * 10)
+            boat = check_ok(boat, take)
     return boat
 
 
 def create_boats():
     take = []
     ships = []
-    boats = [5,4,3,3,2,2]
+    boats = [5, 4, 3, 3, 2, 2]
     for b in boats:
         boat = [-1]
         while boat[0] == -1:
             boat_start = randrange(99)
-            boat_direction = randrange (1,4)
-            print(b,boat_start,boat_direction)
-            boat = check_boat(b,boat_start,boat_direction,take)
+            boat_direction = randrange(1, 5)
+            boat = check_boat(b, boat_start, boat_direction, take)
         ships.append(boat)
-        take = take + boat
-        print(ships)
-        
-    return ships,take
+        take.extend(boat)
+    return ships, take
+
 
 def game_board_comp(take):
     print("           Battleship Game          ")
     print("   0   1   2   3   4   5   6   7   8   9")
-
     place = 0
     for x in range(10):
         row = ""
@@ -69,30 +45,28 @@ def game_board_comp(take):
             ch = " _  "
             if place in take:
                 ch = " o  "
-            row = row + ch
-            place = place + 1
-        
+            row += ch
+            place += 1
         print(x, row)
-        
+
+
 def getting_shot_comp(guesses):
-    
     ok = "n"
     while ok == "n":
         try:
             shot = randrange(99)
-            if shot not in guesses: 
+            if shot not in guesses:
                 ok = "y"
                 guesses.append(shot)
                 break
         except:
             print("Please entry again !")
-            
-    return shot,guesses
+    return shot, guesses
 
-def game_board(hit,miss,comp):
+
+def game_board(hit, miss, comp):
     print("           Battleship Game          ")
     print("   0   1   2   3   4   5   6   7   8   9")
-
     place = 0
     for x in range(10):
         row = ""
@@ -104,34 +78,34 @@ def game_board(hit,miss,comp):
                 ch = " o  "
             elif place in comp:
                 ch = " @  "
-            row = row + ch
-            place = place + 1
+            row += ch
+            place += 1
         print(x, row)
-        
-        
-def check_hit(shot,ships,hit,miss,comp):
-     
-     
+
+
+def check_hit(shot, ships, hit, miss, comp):
+    missed = 1
     for i in range(len(ships)):
         if shot in ships[i]:
             ships[i].remove(shot)
+            missed = 0
             if len(ships[i]) > 0:
                 hit.append(shot)
             else:
-                comp.append(shot)        
-    
-         
-    return ships,hit,miss,comp  
+                comp.append(shot)
+    if missed == 1:
+        miss.append(shot)
+    return ships, hit, miss, comp
 
-boat1 = [45,46,47]
-boat2 = [2,12,22]     
 
 hit = []
 miss = []
 comp = []
 guesses = []
-ships,take = create_boats()
+ships, take = create_boats()
 game_board_comp(take)
-shot,guesses = getting_shot_comp(guesses)
-ships,hit,miss,comp = check_hit(shot,ships,hit,miss,comp)
-game_board(hit,miss,comp)
+
+for i in range(50):
+    shot, guesses = getting_shot_comp(guesses)
+    ships, hit, miss, comp = check_hit(shot, ships, hit, miss, comp)
+    game_board(hit, miss, comp)
