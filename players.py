fddb9587eb52ef3cd820054fa238d6d82204635c
Anderson Padoan <andy_paduano@hotmail.com>
2024-02-10 09:50:56 -0300

    fix function check_ok also create_ship errors

--- players.py
+++ players.py
@@ -14,8 +14,10 @@ def check_ok(boat,take):
                 boat = [-1]
                 break
         if i != 0:   
-            if boat[i+1] != boat[i]+1 or boat[i+1] != boat[i]+10:
-             
+            if boat[i+1] != boat[i]+1 and boat[i+1] != boat[i]+10:
+             boat = [-1]
+             break
+         
     return boat
 
 def get_ship(long,take):
@@ -23,13 +25,14 @@ def get_ship(long,take):
     ok = True
     while ok:
         ship = []
-        print("Enter your Ship")
+        print("Enter your Ship Lenght", long)
         for i in range(long):
             boat_num = input("Please enter a number")
             ship.append(int(boat_num))
             
         ship = check_ok(ship,take)
         if  ship[0] != -1:  
+            take = take + ship
             break
         else:
             print("error, Please enter the number again !")    
@@ -37,7 +40,7 @@ def get_ship(long,take):
     return ship
 
 def create_ships():
-    taken = []
+    take = []
     ships = []
     boats = [5,5,4,3,3,2]
     
